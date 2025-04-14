23bb0667b83ea3ea5baf81570290a9b2c73c4fbb
”Aleksey <117thdragon@gmail.com>
2025-04-14 15:50:01 +0400

    v2.1

--- scripts/deploy_letsencrypt.sh
+++ scripts/deploy_letsencrypt.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-## Variables discription
-# $1=/etc/letsencrypt/archive
-# $2=$DOMAIN
-# $3=privkey.pem                # last modified file
-# $4=cert.pem                   # last modified file
-# $5=fullchain.pem              # last modified file
-
+# Variables
 # Let's Encrypt path
 LPATH=/etc/letsencrypt/archive
-
-# Work direcrory
-WD=/tmp/as
 
 # Install Let's Encrypt
 sudo apt install -y certbot
 
 # Create certificates
 echo "*************************************************************************
-*************************************************************************
+
+
+
 ****  Don't forget to open or redirect port 80 for the OpenVPN Server  **
-*************************************************************************
+
+
+
 *************************************************************************"
 
 echo "Enter domain for create a certificate"
@@ -40,21 +35,26 @@ sudo /usr/local/openvpn_as/scripts/sacli --key "cs.cert" --value_file "$LPATH/$D
 sudo /usr/local/openvpn_as/scripts/sacli --key "cs.ca_bundle" --value_file  "$LPATH/$DOMAIN/$LCHN" ConfigPut
 sudo /usr/local/openvpn_as/scripts/sacli start
 
-# Install and setting up the Let's Encrypt
-while true; do
-    read -p "Do you want install and use Nginx reverse Proxy? (yes/no): " answer
-    if [[ "$answer" == "yes" ]]; then
-        echo "Install and set up Nginx"
-        sudo bash $WD/scripts/deploy_nginx.sh $LPATH $DOMAIN $LPK $LCRT $LCHN
-	sudo bash $WD/scripts/certbotrenew_nginx.sh $LPATH $DOMAIN $LPK $LCRT $LCHN
-	break
-    elif [[ "$answer" == "no" ]]; then
-	sudo bash $WD/script/certbotrenew.sh $LPATH $DOMAIN $LPK $LCRT $LCHN
-	break
-    else
-        echo "Incorrect input. Please enter 'yes' or 'no'"
-    fi
-done
+# Make script for cron
+sudo cat <<EOF >>/usr/local/sbin/certbotrenew_ovpnas.sh
+#!/bin/bash
+
+# Variables
+LPK=$(sudo find $LPATH/$DOMAIN -type f | grep "privkey" | sudo xargs ls -t | head -n 1 | awk -F/ '{print $NF}')
+LCRT=$(sudo find $LPATH/$DOMAIN -type f | grep "cert" | sudo xargs ls -t | head -n 1 | awk -F/ '{print $NF}')
+LCHN=$(sudo find $LPATH/$DOMAIN -type f | grep "chain" | sudo xargs ls -t | head -n 1 | awk -F/ '{print $NF}')
+
+# Renew Let's Encrypt certificate
+certbot renew --renew-by-default
+sleep 15
+
+# Setup new certificate to OVPNAS
+sudo /usr/local/openvpn_as/scripts/sacli --key "cs.priv_key" --value_file "$LPATH/$DOMAIN/$LPK" ConfigPut
+sudo /usr/local/openvpn_as/scripts/sacli --key "cs.cert" --value_file "$LPATH/$DOMAIN/$LCRT" ConfigPut
+sudo /usr/local/openvpn_as/scripts/sacli --key "cs.ca_bundle" --value_file  "$LPATH/$DOMAIN/$LCHN" ConfigPut
+sudo /usr/local/openvpn_as/scripts/sacli start
+
+EOF
 
 # Make file exec
 sudo chmod +x /usr/local/sbin/certbotrenew_ovpnas.sh
